da84ca452517611b401ab721bb8ebffcb15cc60f
Dominique Sakrisson <domsakrisson@gmailcom>
2021-11-02 20:14:50 -0700

    Add cluster to basic setup

--- server.js
+++ server.js
@@ -1,17 +1,54 @@
 const app = require('./index')
-const {createServer} = require('http')
+const cluster = require('cluster')
+const os = require('os');
 const {Server} = require('socket.io')
+const port = process.env.PORT || 3000;
+const http = require('http').Server(app);
+const {setupMaster, setupWorker} = require('@socket.io/sticky');
+const {createAdapter, setupPrimary} = require('@socket.io/cluster-adapter');
+const numCpu = os.cpus().length /2;
 
-const httpServer = createServer(app);
-const io = new Server(httpServer, {});
+if(cluster.isMaster){
+  console.log(`Master ${process.pid} running`);
 
-io.on('connection', (socket) => {
+  //setup sticky sessions
+  setupMaster(http, {
+    loadBalancingMethod: 'least-connection'
+  });
+
+  //setup connections between different workers
+  setupPrimary();
+  cluster.setupMaster({
+    serialization: "advanced",
+  })
+
+  http.listen(port, () => {
+    console.log(`Socket.IO server running  on port :${port}/`);
+  });
+  for(let i=0; i < numCpu; i++){
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork()
+  })
+} else{
+  console.log(`Worker ${process.pid} started`);
+  const io = new Server(http, {});
+
+  io.adapter(createAdapter());
+
+  setupWorker(io);
+
+  io.on('connection', (socket) => {
     console.log('connected to socket');
     
     socket.on('chat message', msg => {
+      console.log('goteeem');
         io.emit('chat message', msg);
         // socket.broadcast.emit('SOMONE SAID: ', msg);
       });
-})
+  })
 
-httpServer.listen(3000);
+}
