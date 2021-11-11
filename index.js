9a47edc375bc9416efe736dbc366025361bd9e8d
Dominique Sakrisson <domsakrisson@gmailcom>
2021-11-10 22:40:16 -0800

    Setup client build for react

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const express = require('express')
 const app = require('express')()
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-app.use(static(path.join(__dirname, 'live-feed-client/build')))
+app.use(express.static(path.join(__dirname, 'live-feed-client/build')))
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/live-feed-client/src/index.js'));
 });
