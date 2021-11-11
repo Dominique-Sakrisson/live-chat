970a6a7f7e14c7ce82f0060e458c828c080d09a6
Dominique Sakrisson <domsakrisson@gmailcom>
2021-11-10 22:22:38 -0800

    Update serving file for react

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/live-feed-client/src/index.js');
 });
 
 module.exports = app
